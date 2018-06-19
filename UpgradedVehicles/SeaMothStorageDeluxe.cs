e5abd34e1d6300e40acf85cdd4859dbc63d06a34
PrimeSonic <prime.sonic@gmail.com>
2018-06-18 21:10:08 -0400

    A mess filled with bugs

---
+++ UpgradedVehicles/SeaMothStorageDeluxe.cs
@@ -0,0 +1,30 @@
+﻿namespace UpgradedVehicles
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using UnityEngine;
+
+    public class SeaMothStorageDeluxe : MonoBehaviour
+    {
+
+        public readonly SeamothStorageContainer[] Storages;
+
+        public SeaMothStorageDeluxe()
+        {
+            Storages = new SeamothStorageContainer[4]
+                        {
+                            new SeamothStorageContainer(),
+                            new SeamothStorageContainer(),
+                            new SeamothStorageContainer(),
+                            new SeamothStorageContainer()
+                        };
+        }
+
+        public ItemsContainer GetStorageInSlot(int slotID)
+        {
+            return Storages[slotID].container;
+        }
+    }
+}
---
+++ UpgradedVehicles/Vehicle_Patcher.cs
@@ -0,0 +1,36 @@
+﻿namespace UpgradedVehicles
+{
+    using Harmony;
+
+    [HarmonyPatch(typeof(Vehicle))]
+    [HarmonyPatch("OnUpgradeModuleChange")]
+    internal class Vehicle_OnUpgradeModuleChange_Patcher
+    {
+        public static void Postfix(Vehicle __instance)
+        {
+            VehicleUpgrader.UpgradeVehicle(__instance);
+        }        
+    }
+
+    [HarmonyPatch(typeof(Vehicle))]
+    [HarmonyPatch("GetStorageInSlot")]
+    internal class Vehicle_GetStorageInSlot_Patcher
+    {
+        public static bool Prefix(ref Vehicle __instance, int slotID, TechType techType, ref ItemsContainer __result)
+        {
+            if (techType == TechType.VehicleStorageModule)
+            {
+                var deluxeStorage = __instance.GetComponent<SeaMothStorageDeluxe>();
+
+                if (deluxeStorage == null)
+                    return true;
+
+                __result = deluxeStorage.GetStorageInSlot(slotID);
+                return false;
+            }
+
+            return true;
+        }
+    }
+
+}
